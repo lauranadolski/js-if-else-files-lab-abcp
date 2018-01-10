585482649f18dc1cdce319145bdebdf45dd74be4
Laura Nadolski <laura.nadolski@gmail.com>
2018-01-10 20:17:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
-
-if (name === "Susan") {
-  console.log ("The name is right on, dood!")
-} else {
-  console.log ("Whoops, I was looking for Susan, is she around?")
-}
-
-
-
+ 
 // Don't worry about this
 module.exports = { name, height
 }
-
